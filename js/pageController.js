531fcacf0d082b71bd2a1bf109e9086a21130272
Eklers <anete.pazusa@gmail.com>
2013-10-01 04:21:22 +0300

    Small fixes

--- js/pageController.js
+++ js/pageController.js
@@ -3,34 +3,35 @@ var active = 'fixpixel';
 function openMenu(id){
 	
 	if(id == 'home'){
-		document.getElementById('home').style.display = 'inline';
-		document.getElementById('fullmenu').style.display = 'none';
+		$("#" + active).fadeOut('fast');
+        $("#home").fadeIn('fast');
+	//	document.getElementById('home').style.display = 'inline';
+	//	document.getElementById('fullmenu').style.display = 'none';
 	}
 	else{
-		console.log('else');
-		document.getElementById('home').style.display = 'none';
-		document.getElementById('fullmenu').style.display = 'block';
+        $("#home").fadeOut('fast');
+    //    $("#fixpixel").fadeIn('fast');
+	//	document.getElementById('home').style.display = 'none';
+	//	document.getElementById('fullmenu').style.display = 'block';
 		
 		if (active == id) {
+			$("#" + active).fadeIn('fast');
 			return;
-			console.log('active == id');
 		}
 		var items = ["we", "plan", "media", "qa"];
 		if (items.indexOf(id) == -1) {
 			return;
-			console.log('index');
 		}
 		if (active == id) {
-			$("#" + active).fadeOut("normal");
+			$("#" + active).fadeOut("fast");
 			return;
 			console.log('active == id');
 		}
 		if (active != null) {
-			$("#" + active).fadeOut("normal");
-			console.log('active != null');
+			$("#" + active).fadeOut("fast");
 		}
 		$("#" + active).fadeOut("slow", function() {
-			$("#" + id).fadeIn("normal");
+			$("#" + id).fadeIn("fast");
 		});
 		active = id;
 		return false;
